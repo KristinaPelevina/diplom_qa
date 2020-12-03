777c10e178cbd8ec0fb929b2d00f3e1453f2a4f6
KristinaPelevina <christya_13.07@mail.ru>
2020-12-03 12:08:45 +0600

    Added java classes, tests

---
+++ src/test/java/ru/netology/page/MainPage.java
@@ -0,0 +1,36 @@
+package ru.netology.page;
+
+import com.codeborne.selenide.SelenideElement;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class MainPage {
+    private SelenideElement debitCardButton = $(byText("Купить"));
+    private SelenideElement creditCardButton = $(byText("Купить в кредит"));
+    private SelenideElement paymentFormName = $(byText("Оплата по карте"));
+    private SelenideElement creditFormName = $(byText("Кредит по данным карты"));
+    private SelenideElement declineMessage = $(withText("Ошибка"));
+    private SelenideElement approveMessage = $(withText("Успешно"));
+
+    public CardPage payWithDebitCard () {
+        debitCardButton.click();
+        paymentFormName.shouldBe(visible);
+        return new CardPage ();
+    }
+
+    public CardPage sendCreditRequest () {
+        creditCardButton.click();
+        creditFormName.shouldBe(visible);
+        return new CardPage();
+    }
+
+    public void checkIfTransactionWasApproved () {
+        approveMessage.waitUntil(visible, 10000);
+    }
+
+    public void checkIfTransactionWasDeclined () {
+        declineMessage.waitUntil(visible,10000);
+    }
+}
